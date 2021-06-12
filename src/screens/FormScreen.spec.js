65924f9acab7b2091d79f337e6d962a73c46d5b5
andresmntr <andres.montero.ranc@alumnos.upm.es>
2021-06-12 02:06:59 +0200

    :white_check_mark: :construction:implementing first tests

---
+++ src/screens/FormScreen.spec.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {render} from '@testing-library/react-native';
+import {FormScreen} from './FormScreen';
+
+// * Create an item
+// * Check input data is valid
+
+it('Should create a submission', () => {
+  const {getByText, getByPlaceholder} = render(<FormScreen />);
+
+  const addItemButton = getByText('Submit');
+  const textInput = getByPlaceholderText('Your first name');
+
+  const createdItemText = 'Andrés';
+
+  fireEvent.changeText(textInput, createdItemText);
+  fireEvent.press(addItemButton);
+
+  const createdItem = getByText(createdItemText);
+
+  expect(createdItem).not.toBeNull();
+});
+
+// it('Should display an error when trying to create an item without any text', () => {
+//   const {getByText} = render(<Submit />);
+
+//   const addItemButton = getByText('Submit');
+
+//   fireEvent.press(addItemButton);
+
+//   const errorMessage = getByText('Please insert a valid text');
+//   const textInput = getByPlaceholderText('');
+
+//   expect(errorMessage).not.toBeNull();
+// });
---
+++ src/screens/Submissions.spec.js
@@ -0,0 +1,2 @@
+// * Should return empty message if there is no submissions
+// * Should show an error whenever a submission state is receibed
